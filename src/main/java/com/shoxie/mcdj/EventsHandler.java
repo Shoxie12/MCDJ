5b699057ffc3d1f13806bd3c546e139fbb615daa
Shoxie12 <65131247+Shoxie12@users.noreply.github.com>
2020-05-17 00:59:38 +0700

    Update to v1.2

--- src/main/java/com/shoxie/mcdj/EventsHandler.java
+++ src/main/java/com/shoxie/mcdj/EventsHandler.java
@@ -18,20 +18,26 @@ public class EventsHandler {
 			e.getName().equals(new ResourceLocation("minecraft", "chests/abandoned_mineshaft")) ||
 			e.getName().equals(new ResourceLocation("minecraft", "chests/simple_dungeon")) ||
 			e.getName().equals(new ResourceLocation("minecraft", "chests/woodland_mansion"))
-			) 
-			updateloot(e.getTable(),"simple_dungeon");
+			)
+			updateChests(e.getTable(),"blankdisc","simple_dungeon");
 		
 		else if(
 				e.getName().equals(new ResourceLocation("minecraft", "entities/zombie")) || 
 				e.getName().equals(new ResourceLocation("minecraft", "entities/zombie_villager"))
 				)
-				updateloot(e.getTable(),"zombie");
+			updateEntityLoot(e.getTable(),"zombie");
 	}
 	
-	
-	private static void updateloot(LootTable table, String tablename) {
+	private static void updateEntityLoot(LootTable table, String tablename) {
 		table.addPool(LootPool.builder().
 				addEntry(TableLootEntry.
 						builder(new ResourceLocation(mcdj.MODID, tablename))).build());
+	}
+    
+	private static void updateChests(LootTable table, String pool, String tablename) {
+		table.addPool(LootPool.builder().
+				addEntry(TableLootEntry.
+						builder(new ResourceLocation(mcdj.MODID, tablename)).weight(1)).
+							name(pool).build());
 	}
 }
