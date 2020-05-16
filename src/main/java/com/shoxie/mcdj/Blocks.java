d8165cabef5e58e41f8ada42e3208efea5aa51c0
Shoxie12 <65131247+Shoxie12@users.noreply.github.com>
2020-05-16 18:31:13 +0700

    Cleaning up...
    Delete unused files

--- src/main/java/com/shoxie/mcdj/Blocks.java
+++
@@ -1,9 +0,0 @@
-package com.shoxie.mcdj;
-
-import com.shoxie.mcdj.block.BlockMusicGenerator;
-
-import net.minecraft.block.Block;
-
-public class Blocks {
-	public static Block MUSIC_GENERATOR = new BlockMusicGenerator();
-}
